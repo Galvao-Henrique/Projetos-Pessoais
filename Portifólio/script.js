7d690eabfcf29e840c8d5c4c20deb43be6dfcedc
Henrique Galvão <henriquepires114@gmail.com>
2025-05-16 18:28:08 -0300

    Atualizaçao 16/05/2025

--- Portifólio/script.js
+++ Portifólio/script.js
@@ -10,3 +10,58 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         });
 });
 /*FIM EMAIL*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Efeito de digitação 
+  const texto = "</Desenvolvedor de Software/>";
+  const elemento = document.getElementById("typewriter");
+  let i = 0;
+
+  function digitar() {
+    if (i < texto.length) {
+      elemento.innerHTML += texto.charAt(i);
+      i++;
+      setTimeout(digitar, 65); // tempo entre letras (em ms)
+    }
+  }
+
+  digitar();
+});
+
+//Partículas 
+tsParticles.load("tsparticles", {
+    background: {
+      color: { value: "#000000" }
+    },
+    particles: {
+      number: {
+        value: 200,
+        density: {
+          enable: true,
+          area: 400
+        }
+      },
+      color: {
+        value: "#ffffff"
+      },
+      shape: {
+        type: "circle"
+      },
+      opacity: {
+        value: 1
+      },
+      size: {
+        value: 0.3
+      },
+      move: {
+        enable: true,
+        speed: {
+          min: -2,
+          max: 9  
+        },
+        direction: "right",
+        straight: true
+      }
+    },
+    detectRetina: true
+  });
